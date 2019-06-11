b7e29a48ebe6905efe4e3a218256ded64190ce54
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-06-11 15:28:30 +0200

    dropdown menu neemt alleen proper namen aan, ook als er sterren zijn zonder

--- code/javascript/starsProperDropdown.js
+++ code/javascript/starsProperDropdown.js
@@ -1,21 +1,30 @@
 // Name: Michael Stroet
 // Student number: 11293284
 
-
 function properDropdown(dataset) {
     /*
-
+    Creates an interactive dropdown menu for selecting stars with proper names
     */
 
-    // Create a dropdown menu for the different stars with proper names
+    // Get all stars with proper names from the dataset
+    var properStarArray = [];
+
+    Object.keys(dataset).forEach(function(star) {
+        if (isNaN(star)) {
+            properStarArray.push(star)
+        };
+    });
+
+    // Create a dropdown menu for the stars
     var properMenu = d3.select("#properDropdown")
 
     properMenu.append("select")
         .selectAll("option")
-        .data(Object.keys(dataset).sort())
+        .data(properStarArray.sort())
         .enter()
         .append("option")
         .attr("value", function (star) {
+            console.log(`Selected star:\n${star}`)
             return star;
         })
         .text(function (star) {

a6bf52f50ee0f13dfcbae8c29fcaabd7bc87a32e
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-06-24 13:50:43 +0200

    javascript comments [6]: Scatterplot

--- code/javascript/starsScatterplot.js
+++ code/javascript/starsScatterplot.js
@@ -1,37 +1,37 @@
 // Name: Michael Stroet
 // Student number: 11293284
 
+// Padding for the scatterplot
+var colorLegendHeight = 30;
+var scatterPadding = {
+    top: 50,
+    right: 50,
+    bottom: 50 + colorLegendHeight,
+    left: 75
+};
+
 function scatterPlot() {
     /*
-    Draws an interactive scatterplot of the star data
-    */
-
-    var colorLegendHeight = 30;
-
-    // Padding for the HRdiagram
-    var padding = {
-        top: 50,
-        right: 50,
-        bottom: 50 + colorLegendHeight,
-        left: 75
-    };
-
+     * Draws an interactive scatterplot of the stars' luminosities and temperatures
+     */
+    // Get the dimensions of the temperature histogram svg
     var svgWidth = document.getElementById("svgHRdiagram").clientWidth;
     var svgHeight = document.getElementById("svgHRdiagram").clientHeight;
 
-    var chartWidth = svgWidth - padding.left - padding.right;
-    var chartHeight = svgHeight - padding.top - padding.bottom;
+    // Determine the dimensions of the scatterplot
+    var scatterWidth = svgWidth - scatterPadding.left - scatterPadding.right;
+    var scatterHeight = svgHeight - scatterPadding.top - scatterPadding.bottom;
 
-    // Select the "svg" for the HRdiagram
+    // Select the scatterplot svg
     var svgScatter = d3.select("#svgHRdiagram");
 
-    // Select the "div" for the tooltip
+    // Select the tooltip div
     var tooltip = d3.select("#HR-diagramTip");
 
-    // Define a "g" for the HRdiagram
+    // Define a "g" tag for the scatterplot
     var scatterPlot = svgScatter.append("g")
         .attr("class", "scatterplot")
-        .attr("transform", `translate(${padding.left}, ${padding.top})`);
+        .attr("transform", `translate(${scatterPadding.left}, ${scatterPadding.top})`);
 
     var stars = Object.entries(originalDataset);
     var properties = Object.values(originalDataset);
@@ -39,32 +39,31 @@ function scatterPlot() {
     var maxTemperature = maxValue(properties, "Temperatuur");
     var minTemperature = minValue(properties, "Temperatuur");
 
-    // Scaling function for x values
+    // Scaling function for the x-values
     var xScale = d3.scaleLinear()
-        .range([0, chartWidth])
+        .range([0, scatterWidth])
         .domain([maxTemperature * 1.1, minTemperature * 0.9]);
 
-    // Scaling function for y values
+    // Scaling function for the y-values
     var yScale = d3.scaleLog()
-        .range([0, chartHeight])
+        .range([0, scatterHeight])
         .domain([maxValue(properties, "Lichtkracht") * 1.1, minValue(properties, "Lichtkracht") * 0.9]);
 
     // Draw x-axis
     scatterPlot.append("g")
         .call(d3.axisBottom(xScale)
             .ticks(6)
-            // .tickValues([3700, 5200, 6000, 7500, 10000, 30000]) // M K G F A B O
             .tickFormat(d3.format("d"))
         )
         .attr("class", "axis")
         .attr("id", "x")
-        .attr("transform", `translate(0, ${chartHeight + colorLegendHeight})`);
+        .attr("transform", `translate(0, ${scatterHeight + colorLegendHeight})`);
 
-    // Draw x label
+    // Draw x-label
     svgScatter.append("text")
         .attr("class", "label")
-        .attr("x", chartWidth / 2 + padding.left)
-        .attr("y", chartHeight + padding.top + padding.bottom / 1.05)
+        .attr("x", scatterWidth / 2 + scatterPadding.left)
+        .attr("y", scatterHeight + scatterPadding.top + scatterPadding.bottom / 1.05)
         .attr("text-anchor", "middle")
         .text("Effectieve temperatuur (K)");
 
@@ -73,7 +72,7 @@ function scatterPlot() {
         .attr("class", "grid")
         .attr("opacity", 0.15)
         .call(d3.axisBottom(xScale)
-            .tickSize(chartHeight, 0, 0)
+            .tickSize(scatterHeight, 0, 0)
             .tickFormat("")
     );
 
@@ -82,29 +81,29 @@ function scatterPlot() {
         .attr("class", "axis")
         .attr("id", "y");
 
-    // Draw y label
+    // Draw y-label
     svgScatter.append("text")
         .attr("class", "label")
-        .attr("x", - (chartHeight / 2) - padding.top)
-        .attr("y", padding.left / 6)
+        .attr("x", - (scatterHeight / 2) - scatterPadding.top)
+        .attr("y", scatterPadding.left / 6)
         .attr("transform", "rotate(270)")
         .attr("text-anchor", "middle")
-        .text("Lichtkracht (zon = 1)");
+        .text("Lichtkracht (zonslichtkrachten)");
 
     // Draw horizontal gridlines
     scatterPlot.append("g")
         .attr("class", "grid")
         .attr("opacity", 0.15)
         .call(d3.axisRight(yScale)
-            .tickSize(chartWidth, 0, 0)
+            .tickSize(scatterWidth, 0, 0)
             .tickFormat("")
         );
 
     // Draw title
     svgScatter.append("text")
         .attr("class", "title")
-        .attr("x", chartWidth / 2 + padding.left)
-        .attr("y", padding.top / 1.5)
+        .attr("x", scatterWidth / 2 + scatterPadding.left)
+        .attr("y", scatterPadding.top / 1.5)
         .attr("text-anchor", "middle")
         .text("Hertzsprung-Russell diagram");
 
@@ -149,7 +148,7 @@ function scatterPlot() {
             });
 
 
-    // Padding for the legend
+    // Padding for the color legend
     var legendPadding = {
             top   : 2,
             right : 0,
@@ -157,7 +156,7 @@ function scatterPlot() {
             left  : 0
         };
 
-    // Append a "defs" tag to g
+    // Append a "defs" tag to the scatterplot svg
     var defs = svgScatter.append("defs")
         .attr("class", "linearGradient");
 
@@ -165,7 +164,7 @@ function scatterPlot() {
     var linearGradient = defs.append("linearGradient")
         .attr("id", "scatterGradient");
 
-    //Horizontal gradient
+    // Horizontal gradient from right to left
     linearGradient
         .attr("x1", "100%")
         .attr("y1", "0%")
@@ -187,13 +186,13 @@ function scatterPlot() {
     // Define a "g" for the legend
     var legend = svgScatter.append("g")
         .attr("class", "legend")
-        .attr("transform", `translate(${padding.left}, ${padding.top + chartHeight})`)
+        .attr("transform", `translate(${scatterPadding.left}, ${scatterPadding.top + scatterHeight})`)
 
     // Add the gradient to the legend
     legend.append("rect")
         .attr("x", legendPadding.left)
         .attr("y", legendPadding.top)
-        .attr("width", chartWidth - legendPadding.left - legendPadding.right)
+        .attr("width", scatterWidth - legendPadding.left - legendPadding.right)
         .attr("height", colorLegendHeight - legendPadding.top - legendPadding.bottom)
         .style("fill", "url(#scatterGradient)")
         .style("stroke", "black");
@@ -201,29 +200,23 @@ function scatterPlot() {
 
 function updateHRDiagram(newDataset) {
     /*
-     *
+     * Updates the scatterplot with the new dataset
      */
-     var colorLegendHeight = 30;
-
-     // Padding for the HRdiagram
-     var padding = {
-         top: 50,
-         right: 50,
-         bottom: 50 + colorLegendHeight,
-         left: 75
-     };
-
+    // Get the dimensions of the temperature histogram svg
     var svgWidth = document.getElementById("svgHRdiagram").clientWidth;
     var svgHeight = document.getElementById("svgHRdiagram").clientHeight;
 
-    var chartWidth = svgWidth - padding.left - padding.right;
-    var chartHeight = svgHeight - padding.top - padding.bottom;
+    // Determine the dimensions of the scatterplot
+    var scatterWidth = svgWidth - scatterPadding.left - scatterPadding.right;
+    var scatterHeight = svgHeight - scatterPadding.top - scatterPadding.bottom;
 
-    // Select the "svg" for the HRdiagram
+    // Select the scatterplot svg
     var svgScatter = d3.select("#svgHRdiagram");
-    var scatterPlot = svgScatter.select(".scatterplot");
 
-    // Select the "div" for the tooltip
+    // select the scatterplot g
+    var scatterPlot = svgScatter.select(".scatterplot")
+
+    // Select the tooltip div
     var tooltip = d3.select("#HR-diagramTip");
 
     var stars = Object.entries(newDataset);
@@ -234,33 +227,34 @@ function updateHRDiagram(newDataset) {
 
     // Scaling function for x values
     var xScale = d3.scaleLinear()
-        .range([0, chartWidth])
+        .range([0, scatterWidth])
         .domain([maxTemperature * 1.1, minTemperature * 0.9]);
 
     // Scaling function for y values
     var yScale = d3.scaleLog()
-        .range([0, chartHeight])
+        .range([0, scatterHeight])
         .domain([maxValue(properties, "Lichtkracht") * 1.1, minValue(properties, "Lichtkracht") * 0.9]);
 
-    // Draw x-axis
+    // Update x-axis
     scatterPlot.select(".axis#x")
         .transition()
         .duration(transitionDuration)
         .call(d3.axisBottom(xScale)
             .ticks(6)
-            // .tickValues([3700, 5200, 6000, 7500, 10000, 30000]) // M K G F A B O
             .tickFormat(d3.format("d"))
         );
 
-    // Draw y-axis
+    // Update y-axis
     scatterPlot.select(".axis#y")
         .transition()
         .duration(transitionDuration)
         .call(d3.axisLeft(yScale));
 
+    // Replace the old data with the new bins
     var points = scatterPlot.selectAll(".star")
         .data(stars)
 
+    // Update the existing points with a new id, position, color and radius
     points.transition()
         .duration(transitionDuration)
         .attr("id", star => {return "Star_" + star[0].replace(/\./g, '-').replace(/ /g, '_').replace(/\'/g, '')})
@@ -277,6 +271,7 @@ function updateHRDiagram(newDataset) {
             return 2 + Math.pow(star[1]["Straal"], 1/3);
         });
 
+    // Create new points from the dataset
     points.enter()
         .append("circle")
             .attr("class", "star")
@@ -295,7 +290,6 @@ function updateHRDiagram(newDataset) {
             .attr("r", 0)
             .on("click", function(star) {
                 highlightStar(star);
-                showStarInfo(star);
             })
             .on("mousemove", function(star) {
                 tooltip
@@ -320,16 +314,17 @@ function updateHRDiagram(newDataset) {
                 return 2 + Math.pow(star[1]["Straal"], 1/3);
             });
 
+    // Remove points no longer in the dataset
     points.exit()
         .transition()
         .duration(transitionDuration)
         .attr("r", 0)
         .remove();
 
-    // Add a linearGradient element to the defs
+    // Select the linear gradient
     var linearGradient = d3.select("#scatterGradient");
 
-    // Create a continuous color legend
+    // Update the legend with new colors
     linearGradient.selectAll("stop")
         .data(createGradientData(minTemperature, maxTemperature))
         .attr("offset", function(data) {
@@ -338,5 +333,4 @@ function updateHRDiagram(newDataset) {
         .attr("stop-color", function(data) {
             return data.color;
         });
-
 };
